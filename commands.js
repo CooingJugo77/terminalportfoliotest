b3f8604a4511bee3c4b61fcd3a9336dce97771fc
Hugo Lindberg <51190262+CooingJugo77@users.noreply.github.com>
2024-07-02 20:10:55 +0200

    Added final files

---
+++ commands.js
@@ -0,0 +1,65 @@
+var youtube = "https://www.youtube.com/@thecodingyoungster";
+var twitter = "https://twitter.com/CooingJugo77";
+var password = "password";
+var github = "https://github.com/CooingJugo77";
+var email = "hugolindberg07@icloud.com";
+var website = "https://hugolindberg.me";
+
+whois = [
+  "<br>",
+  "Hello, I'm Hugo!👋",
+  "My dream hob is to be a game/website developer.",
+  "<br>",
+];
+
+whoami = ["<br>", "visitor.", "<br>"];
+
+social = [
+  "<br>",
+  '📺 YouTube: <a href="' +
+    youtube +
+    '" target="_blank">youtube/@thecodingyoungster</a>',
+  '🐦 Twitter: <a href="' +
+    twitter +
+    '" target="_blank">twitter/@CooingJugo77</a>',
+  '📂 GitHub: <a href="' + github + '" target="_blank">github/CooingJugo77</a>',
+  '🌐 Website: <a href="' + website + '" target="_blank">hugolindberg.me</a>',
+  "<br>",
+];
+
+secret = [
+  "<br>",
+  '<span class="command">sudo</span>                Only use if you\'re admin',
+  "<br>",
+];
+
+projects = [
+  "<br>",
+  "Most of my projects are on my GitHub and my website.",
+  "<br>",
+];
+
+help = [
+  "<br>",
+  '<span class="command">whois</span>              Who am I?',
+  '<span class="command">whoami</span>             Who are you?',
+  '<span class="command">video</span>              View my videos',
+  '<span class="command">social</span>             View my socials',
+  '<span class="command">projects</span>           View my projects',
+  '<span class="command">history</span>            View command history',
+  '<span class="command">help</span>               You should already know what this does',
+  '<span class="command">email</span>              View my email',
+  '<span class="command">clear</span>              Clear the terminal',
+  '<span class="command">banner</span>             View the "welcome" message',
+  '<span class="command">pwd</span>                Print working directory',
+  '<span class="command">ls</span>                 Display a list of files/folders',
+  '<span class="command">cd</span>                 Change directory',
+  "<br>",
+];
+
+banner = [
+  '<h1 class="index">Hugo LINDBERG | All rights reserved.</h1>',
+  '<span class="color2">Welcome to my portfolio test with an interactive terminal.</span>',
+  '<span class="color2"></span>To get a list of available commands, type <span class="command">help</span><span class="color2">.</span></span>',
+  "<br>",
+];
